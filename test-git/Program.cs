cb2df8d120797b219c12445a3ff3405b957231f8
FerasBinHussain <ferasbinhussain@gmail.com>
2022-06-21 11:39:57 +0300

    test dev

--- test-git/Program.cs
+++ test-git/Program.cs
@@ -15,6 +15,24 @@ namespace test_git
             Console.WriteLine("From Dev branch");
 
 
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("test!");
+            Console.WriteLine("test2!");
+            Console.WriteLine("Test from Nasser!");
+            Console.WriteLine("test Conf");
+
+            Console.WriteLine("From Dev branch");
+
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("test!");
+            Console.WriteLine("test2!");
+            Console.WriteLine("Test from Nasser!");
+            Console.WriteLine("test Conf");
+
+            Console.WriteLine("From Dev branch");
+
+
         }
     }
 }
